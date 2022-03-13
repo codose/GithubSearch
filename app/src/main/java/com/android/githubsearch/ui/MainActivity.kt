45f2ac75563652292e03766974999b76a2c1cc5d
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2022-03-13 17:14:29 +0100

    Updates ReadMe.Md

--- app/src/main/java/com/android/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/android/githubsearch/ui/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import com.android.githubsearch.ui.composable.SearchComponent
@@ -35,6 +40,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column(Modifier.padding(16.dp)) {
+                        Box(modifier = Modifier.fillMaxWidth()) {
+                            Text(modifier = Modifier.padding(16.dp), text = "Github Search", fontSize = 20.sp, fontWeight = FontWeight.Black)
+                        }
                         SearchComponent {
                             searchViewModel.searchUser(it)
                         }
